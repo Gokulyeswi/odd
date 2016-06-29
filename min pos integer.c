b074e935a26a791c229e75a388d4a7722a2da6b1
Gokula Krishnan <gokulyeswanth@gmail.com>
2016-06-29 14:36:28 +0530

    finish

---
+++ min pos integer.c
@@ -0,0 +1,22 @@
+# odd
+#include <stdio.h>
+void odd(int c)
+{
+	if(c%2==1)
+	{
+		printf("the minimum positive integer is %d",c);
+	}
+}
+main()
+{
+int n,i,c;
+printf("Enter number");
+scanf("%d",&n);
+for(i=0;i<n-1;i++);
+{
+	if(n/i==c);
+	{
+		odd(c);
+	}
+}
+}
